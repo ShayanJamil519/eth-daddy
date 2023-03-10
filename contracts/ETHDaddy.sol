708b2eaa60ab5ed916e890f3a3daaa9c264079e4
ShayanJamil519 <shayanjamil500@gmail.com>
2023-03-10 12:29:11 +0500

    Withdraw Funds Functionality Implemented and Tested

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ETHDaddy is ERC721 {
-    uint256 public maxSupply;
+    uint256 public maxSupply;   // max number of nfts created
+    uint256 public totalSupply;  // total nfts created at particular time
     address public owner;
 
     struct Domain{
@@ -32,12 +33,32 @@ contract ETHDaddy is ERC721 {
 
     }
 
+    function mint (uint256 _id) public payable {
+        require(_id != 0, "Can't mint unlisted domains");
+        require(_id <= maxSupply , "Can't mint more domains than maxSupply");
+        require(domains[_id].isOwned == false, "Domain already owned");
+        require(msg.value >= domains[_id].cost, "Less cost price of domain is given");
+
+        domains[_id].isOwned = true;
+        totalSupply++;
+
+        _safeMint(msg.sender, _id);
+    }
+
+    function withdraw() public onlyOwner{
+        (bool success, ) = owner.call{value:address(this).balance}("");
+        require(success);
+    }
+
 
     // getters
     function getDomain(uint256 _id) public view returns(Domain memory){
         return domains[_id];
     }
 
+    function getBalance() public view returns(uint256){
+        return address(this).balance;
+    }
 
 
 }
--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -45,6 +45,10 @@ describe("ETHDaddy", () => {
       const result = await ethDaddy.maxSupply();
       expect(result).to.equal(1);
     });
+    it("Returns the total supply at deploment", async () => {
+      const result = await ethDaddy.totalSupply();
+      expect(result).to.equal(0);
+    });
   });
   describe("Domain", () => {
     it("Returns domain attributes", async () => {
@@ -52,6 +56,66 @@ describe("ETHDaddy", () => {
       expect(domain.name).to.be.equal("jack.eth");
       expect(domain.cost).to.be.equal(tokens(10));
       expect(domain.isOwned).to.be.equal(false);
+    });
+  });
+
+  describe("Minting", () => {
+    const ID = 1;
+    const AMOUNT = tokens(10);
+
+    beforeEach(async () => {
+      const transaction = await ethDaddy
+        .connect(owner1)
+        .mint(ID, { value: AMOUNT });
+      await transaction.wait();
+    });
+
+    it("Updates the owner", async () => {
+      const owner = await ethDaddy.ownerOf(ID);
+      expect(owner).to.be.equal(owner1.address);
+    });
+
+    it("Updates the domain status", async () => {
+      const domain = await ethDaddy.getDomain(ID);
+      expect(domain.isOwned).to.be.equal(true);
+    });
+
+    it("Updates the total supply after minting", async () => {
+      const result = await ethDaddy.totalSupply();
+      expect(result).to.be.equal(1);
+    });
+
+    it("Updates the contract balance", async () => {
+      const result = await ethDaddy.getBalance();
+      expect(result).to.be.equal(AMOUNT);
+    });
+  });
+
+  describe("Withdrawing", () => {
+    const ID = 1;
+    const AMOUNT = tokens(10);
+    let balanceBefore;
+
+    beforeEach(async () => {
+      balanceBefore = await ethers.provider.getBalance(deployer.address);
+
+      let transaction = await ethDaddy
+        .connect(owner1)
+        .mint(ID, { value: AMOUNT });
+      await transaction.wait();
+
+      transaction = await ethDaddy.connect(deployer).withdraw();
+      await transaction.wait();
+    });
+
+    it("Updates the owner/deployer balance", async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address);
+      expect(balanceAfter).to.be.greaterThan(balanceBefore);
+    });
+
+    it("Updates the contract balance", async () => {
+      const result = await ethDaddy.getBalance();
+      expect(result).to.be.equal(0);
     });
   });
 });
