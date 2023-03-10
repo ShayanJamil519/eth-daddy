f7706f932f1b6d26fdae3b0474e4c90abec6229c
ShayanJamil519 <shayanjamil500@gmail.com>
2023-03-10 09:43:55 +0500

    ERC721 Constructor inherited and deployment tested

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -1,4 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract ETHDaddy {}
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ETHDaddy is ERC721 {
+    // string public name="ETH Daddy";
+
+    constructor(string memory _name,string memory _symbol)
+    ERC721(_name, _symbol){
+
+    }
+
+
+
+}
--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,29 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("ETHDaddy", () => {
+  let ethDaddy;
+  const NAME = "ETH Daddy";
+  const SYMBOL = "ETHD";
 
-})
+  beforeEach(async () => {
+    const ETHDaddy = await ethers.getContractFactory("ETHDaddy");
+    ethDaddy = await ETHDaddy.deploy(NAME, SYMBOL);
+  });
+
+  describe("Deployment", () => {
+    it("has a name", async () => {
+      const result = await ethDaddy.name();
+      expect(result).to.equal(NAME);
+    });
+
+    it("has a symbol", async () => {
+      const result = await ethDaddy.symbol();
+      expect(result).to.equal(SYMBOL);
+    });
+  });
+});
