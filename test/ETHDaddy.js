1bfc57bf82056afc0b72d08eb1111862144b1218
ShayanJamil519 <shayanjamil500@gmail.com>
2023-03-10 11:11:13 +0500

    Domain Listing and Testing Done

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -7,12 +7,23 @@ const tokens = (n) => {
 
 describe("ETHDaddy", () => {
   let ethDaddy;
+  let deployer, owner1;
   const NAME = "ETH Daddy";
   const SYMBOL = "ETHD";
 
   beforeEach(async () => {
+    // Setup Accounts
+    [deployer, owner1] = await ethers.getSigners();
+
+    // Deploying Contract
     const ETHDaddy = await ethers.getContractFactory("ETHDaddy");
     ethDaddy = await ETHDaddy.deploy(NAME, SYMBOL);
+
+    // Lists Domain
+    const transaction = await ethDaddy
+      .connect(deployer)
+      .list("jack.eth", tokens(10));
+    await transaction.wait();
   });
 
   describe("Deployment", () => {
@@ -24,6 +35,23 @@ describe("ETHDaddy", () => {
     it("has a symbol", async () => {
       const result = await ethDaddy.symbol();
       expect(result).to.equal(SYMBOL);
+    });
+
+    it("Sets the owner", async () => {
+      const result = await ethDaddy.owner();
+      expect(result).to.equal(deployer.address);
+    });
+    it("Returns the max supply", async () => {
+      const result = await ethDaddy.maxSupply();
+      expect(result).to.equal(1);
+    });
+  });
+  describe("Domain", () => {
+    it("Returns domain attributes", async () => {
+      let domain = await ethDaddy.getDomain(1);
+      expect(domain.name).to.be.equal("jack.eth");
+      expect(domain.cost).to.be.equal(tokens(10));
+      expect(domain.isOwned).to.be.equal(false);
     });
   });
 });
